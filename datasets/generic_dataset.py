8e377c57ebab64c2bc3c9244fb3de471a6539dee
Frederik Warburg <frederikwarburg@Frederiks-MacBook-Pro-10.local>
2020-05-13 10:55:03 +0200

    initial commit

---
+++ datasets/generic_dataset.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset
+import pandas as pd
+from os.path import join
+from sklearn.neighbors import NearestNeighbors
+import math
+import torch
+import random
+
+class ImagesFromList(Dataset):
+	def __init__(self, images, transform):
+
+	    self.images = np.asarray(images)
+	    self.transform = transform
+
+	def __len__(self):
+	    return len(self.images)
+
+	def __getitem__(self, idx):
+		
+		img = Image.open(self.images[idx])
+		img = self.transform(img)
+
+		return img, idx
+
+
---
+++ utils/utils.py
@@ -0,0 +1,13 @@
+from torchvision import transforms
+import torch
+
+def configure_transform(image_dim, meta):
+
+	normalize = transforms.Normalize(mean=meta['mean'], std=meta['std'])
+	transform = transforms.Compose([
+		transforms.Resize(image_dim),
+		transforms.ToTensor(),
+		normalize,
+	])
+
+	return transform
