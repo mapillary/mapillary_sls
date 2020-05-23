61c8b344a843a34b57acda4f23bed87fc44ef09d
Manuel López Antequera <mlopezantequera@gmail.com>
2020-05-23 17:32:42 +0200

    Update evaluate.py to auto-download sample + misc

--- evaluate.py
+++ evaluate.py
@@ -1,41 +1,30 @@
+import argparse
+import urllib
+import zipfile
+from os.path import basename as bn
+from pathlib import Path
+
+import numpy as np
+
 from datasets.msls import MSLS
 from utils.evaluate import evaluate
-import numpy as np
-from pathlib import Path
-import argparse
-from os.path import basename as bn
 
-def create_dummy_predictions(prediction_path, dataset):
-    print("==> Prediction file doesn't exist")
-    print("==> We create a new dummy prediction file at :")
-    print("==> ", prediction_path)
 
-    numQ = len(dataset.qIdx)
-    numDb = len(dataset.dbImages)
+def download_msls_sample(path):
+    print("Downloading MSLS sample to {}".format(path))
+    path.mkdir(parents=True)
+    path_dl, _ = urllib.request.urlretrieve("https://static.mapillary.com/MSLS_samples.zip")
+    with zipfile.ZipFile(path_dl, 'r') as zf:
+        zf.extractall(path)
 
-    # all keys in the database and query keys
-    query_keys = np.asarray([bn(key)[:-4] for key in dataset.qImages[dataset.qIdx]]).reshape(numQ,1)
-    database_keys = [bn(key)[:-4] for key in dataset.dbImages]
-    
-    # choose n = min(5, numDb) random elements from the database
-    ranks = np.asarray([np.random.choice(database_keys, replace=False, size = (min(5, numDb))) for q in range(numQ)])
 
-    # save the dummy predictions
-    first_row_str = ' '.join([str(i) for i in ranks[0][:-1]]) + ' and ' + str(ranks[0][-1]) + '.'
-    np.savetxt(prediction_path, np.concatenate([query_keys, ranks], axis=1), fmt='%s',
-               header="Each row contains the a query key followed by N prediction keys\n"
-                      "The format is valid for all tasks (im2im, im2seq, ...).\n"
-                      
-                      "For example, in this file, the image/sequence with key " + query_keys[0,0] + 
-                      " is predicted to match with " + first_row_str)
-
-if __name__ == "__main__":
+def main():
 
     parser = argparse.ArgumentParser()
-    root_default = Path(__file__).parent / 'data'
-    parser.add_argument('prediction',
+    root_default = Path(__file__).parent / 'MSLS_sample'
+    parser.add_argument('--prediction',
                         type=Path,
-                        default=Path(__file__).parent / 'msls_prediction.csv',
+                        default='example_msls_prediction.csv',
                         help='Path to the prediction to be evaluated')
     parser.add_argument('--msls-root',
                         type=Path,
@@ -57,6 +46,12 @@ if __name__ == "__main__":
                              '[all, s2w, w2s, o2n, n2o, d2n, n2d]')
     args = parser.parse_args()
 
+    if not args.msls_root.exists():
+        if args.msls_root == root_default:
+            download_msls_sample(args.msls_root)
+        else:
+            raise FileNotFoundError("Not found: {}".format(args.msls_root))
+
     # select for which ks to evaluate
     ks = [1, 5, 10, 20]
 
@@ -67,9 +62,6 @@ if __name__ == "__main__":
     positive_keys = [[bn(key)[:-4] for key in dataset.dbImages[pos]] for pos in dataset.pIdx]
     query_keys = [bn(key)[:-4] for key in dataset.qImages[dataset.qIdx]]
 
-    # create dummy predictions
-    create_dummy_predictions(args.prediction, dataset)
-    
     # load prediction rankings
     predictions = np.loadtxt(args.prediction, ndmin=2, dtype=str)
     
@@ -83,3 +75,5 @@ if __name__ == "__main__":
         print()
 
 
+if __name__ == "__main__":
+    main()
