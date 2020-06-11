20830ec08411b1103b25e7192b5eb8220393a964
Frederik Warburg <frewar1905@gmail.com>
2020-06-11 14:50:34 +0200

    add extra checks to the evaluation script

--- evaluate.py
+++ evaluate.py
@@ -65,7 +65,8 @@ def main():
     dataset = MSLS(args.msls_root, cities = args.cities, mode = 'val', posDistThr = args.threshold, 
                     task = args.task, seq_length = args.seq_length, subtask = args.subtask)
 
-    # get query and positive image keys    
+    # get query and positive image keys  
+    database_keys =  [','.join([bn(i)[:-4] for i in p.split(',')]) for p in dataset.dbImages]
     positive_keys = [[','.join([bn(i)[:-4] for i in p.split(',')]) for p in dataset.dbImages[pos]] for pos in dataset.pIdx]
     query_keys = [','.join([bn(i)[:-4] for i in p.split(',')]) for p in dataset.qImages[dataset.qIdx]]
     
@@ -79,6 +80,17 @@ def main():
     # Ensure that there is a prediction for each query image
     for k in query_keys:
         assert k in predictions[:, 0], "You didn't provide any predictions for image {}".format(k)
+
+    # Ensure that all predictions are in database
+    for k in predictions[:, 1:]:
+        assert np.in1d(k, database_keys).all(), "Some of your predictions are not in the database for the selected task {}".format(k)
+
+    # Ensure that all predictions are unique
+    for k in range(len(query_keys)):
+        assert len(predictions[k, 1:]) == len(np.unique(predictions[k, 1:])), "You have douplicate predictions for image {}".format(query_keys[k])
+
+    # Ensure that all queries are unique
+    assert len(predictions[:,0]) == len(np.unique(predictions[:,0])), "You have douplicate query images"
 
     # Check if there are predictions that don't correspond to any query images
     for k in predictions[:, 0]:
